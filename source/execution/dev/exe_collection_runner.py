f7804a5210865c6a9da1fdc93ee1afc655948cda
Christian Krullikowski <christian.krullikowski@dlr.de>
2023-10-06 08:19:28 +0200

    Jupyter notebooks

---
+++ source/execution/dev/exe_collection_runner.py
@@ -0,0 +1,43 @@
+from source.execution.exe_config import Config
+from source.execution.exe_production import Production
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    orbits = [
+        "asc",
+        "des"
+    ]
+
+    pol = "VH"
+
+    aois = [
+        # "munich_airport",
+        # "munich_ikea"
+        # "volvo_gent",
+        # "bmw_regensburg",
+        # "bmw_leipzig",
+        # "vw_emden",
+        # "vw_wolfsburg",
+        # "vw_zwickau",
+        # "opel_ruesselsheim",
+        "porsche_leipzig",
+        # "ford_koeln",
+    ]
+    for orbit in orbits:
+        for aoi in aois:
+            conf = Config(
+                aoi_dir=Path(r"input/AOIs"),
+                working_dir=Path.home().joinpath("sat4ec"),
+                out_dir=Path(r"output"),
+                orbits=orbit,
+                pols=pol,  # only use VH polarization
+                aoi="porsche_leipzig",
+                ext="geojson",
+                start="2016-01-01",
+                monthly=False,
+                regression="spline",
+                linear=True,
+            )
+            prod = Production(config=conf)
+            prod.workflow()
---
+++ source/execution/exe_production.py
@@ -0,0 +1,36 @@
+import subprocess
+from source.execution.exe_helper_functions import get_name
+
+
+class Production:
+    def __init__(self, config=None):
+        self.config = config
+
+    def workflow(self):
+        response = subprocess.run(
+            [
+                "python3",
+                "../source/main.py",
+                "--aoi_data",
+                self.config.aoi,
+                "--out_dir",
+                self.config.out_dir,
+                "--start_date",
+                f"{self.config.start}",
+                # "--end_date",
+                # f"{self.config.end}",
+                "--orbit",
+                self.config.orbit,
+                "--polarization",
+                self.config.pol,
+                "--name",
+                get_name(self.config.aoi_name),
+                "--aggregate",
+                "monthly" if self.config.monthly else "daily",
+                "--regression",
+                self.config.regression,
+                "--linear",
+                "true" if self.config.linear else "false",
+            ],
+            capture_output=False,
+        )
